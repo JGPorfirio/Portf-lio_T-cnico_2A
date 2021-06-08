5fc4329e0e57f010518a7139158bcf7b198f2385
JGPorfirio <83706348+JGPorfirio@users.noreply.github.com>
2021-06-08 17:07:44 -0300

    Update At6.sh

--- FundamentosTI/Exemplos/At6.sh
+++ FundamentosTI/Exemplos/At6.sh
@@ -2,27 +2,32 @@
 # Autor: João Guilherme da Silva Porfirio
 # Descrição: Irá pegar um valor(N) e imprimir todos os numeros entre 1(inclusive) até N(inclusive). Caso valor for menor que zero, irá ser pedido outro valor.
 
-echo "Digite um valor"
+echo "Digite um  valor!"
 read n
 
 if [ $n -gt 0 ]
 then
+
         for i in $(seq 1 $n)
         do
-              echo "$i"
+                echo "$i"
         done
+
 elif [ $n -lt 0 ]
-then 
-        echo "Valor invalido por favor, digete um valor maior que zero!"
+then
+
+        echo "Valor invalido por favor, digite um valor maior que 0"
         read y
-        
+
         if [ $y -gt 0 ]
-        then
-            for x in $(seq 1 $y)
-        do
-              echo "$x"
-        done
-else
-        echo "O valor informado é menor que zero, por favor execute o codigo novamente"
-fi
+        then    
+
+                for x in $(seq 1 $y)
+                do
+                        echo "$x"
+                done
+
+        else
+                echo "O valor informado é menor que zero, por favor execute o codigo novamente"
+        fi
 fi
